e93c9158f1efa23c1d58b2dad098bea72f8a9598
Lautaro Scuffi <scuffilautaro@gmail.com>
2023-12-03 21:41:59 -0300

    genera portfolio

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+const isDarkMode = localStorage.getItem("darkMode") === "enabled";
+if (isDarkMode) {
+  body.classList.add("dark-mode");
+  darkModeToggle.checked = true;
+}
+darkModeToggle.addEventListener("change", () => {
+  if (darkModeToggle.checked) {
+    body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", "disabled");
+  }
+});
+window.location.pathname == "/src/pages/portfolio/portfolio.html"
+  ? (body.style.overflow = "auto")
+  : (body.style.overflow = "hidden");
